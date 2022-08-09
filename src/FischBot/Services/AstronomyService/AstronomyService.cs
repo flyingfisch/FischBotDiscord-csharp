28c1ab96357cd69d025f1d46b8f157c4d0ef6c12
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2022-08-09 09:22:08 -0400

    working on updating astro apod

--- src/FischBot/Services/AstronomyService/AstronomyService.cs
+++ src/FischBot/Services/AstronomyService/AstronomyService.cs
@@ -23,6 +23,7 @@ namespace FischBot.Services.AstronomyService
                 Name = response.Title,
                 Caption = response.Explanation,
                 Url = response.Url,
+                MediaType = response.Media_Type,
                 Date = response.Date
             };
 
