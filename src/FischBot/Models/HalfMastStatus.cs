749b3f917f26a34facf098d68addd609761eb4e2
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2023-10-04 16:17:28 -0400

    updated the half mast command to get the flag status from halfmast.org since twitter is now charging $100/mo for their API

--- src/FischBot/Models/HalfMastStatus.cs
+++ src/FischBot/Models/HalfMastStatus.cs
@@ -1,7 +1,8 @@
 namespace FischBot.Models
 {
-    public class HalfMastNotice
+    public class HalfMastStatus
     {
+        public bool IsHalfStaff { get; set; }
         public string State { get; set; }
         public string Description { get; set; }
         public string SourceUrl { get; set; }
